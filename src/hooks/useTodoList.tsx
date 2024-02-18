f52eb10c303c7ca1674ecd7e4de6d401402af7b6
Jaeha Lee <wogkdkrm112@gmail.com>
2024-02-18 23:07:31 +0900

    feat: asyncStorage 적용해서 todo 불러오기

--- src/hooks/useTodoList.tsx
+++ src/hooks/useTodoList.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import dayjs, { Dayjs } from 'dayjs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface useTodoListProps {
   selectedDate: Dayjs;
@@ -12,18 +13,24 @@ export interface TodoItemType {
   isSuccess: boolean;
 }
 
-const defaultTodoList = [
-  { id: 1, content: 'react', date: dayjs(), isSuccess: true },
-  { id: 2, content: 'docker', date: dayjs(), isSuccess: true },
-  { id: 3, content: 'aws', date: dayjs(), isSuccess: false },
-  { id: 4, content: 'Java', date: dayjs(), isSuccess: true },
-  { id: 5, content: 'React-Native', date: dayjs(), isSuccess: false },
-  { id: 6, content: 'tamagui', date: dayjs(), isSuccess: false },
+const defaultTodoList: TodoItemType[] = [
+  // { id: 1, content: 'react', date: dayjs(), isSuccess: true },
+  // { id: 2, content: 'docker', date: dayjs(), isSuccess: true },
+  // { id: 3, content: 'aws', date: dayjs(), isSuccess: false },
+  // { id: 4, content: 'Java', date: dayjs(), isSuccess: true },
+  // { id: 5, content: 'React-Native', date: dayjs(), isSuccess: false },
+  // { id: 6, content: 'tamagui', date: dayjs(), isSuccess: false },
 ];
 
 export default function useTodoList({ selectedDate }: useTodoListProps) {
-  const [todoList, setTodoList] = React.useState(defaultTodoList);
+  const [todoList, setTodoList] = React.useState<TodoItemType[]>(defaultTodoList);
   const [input, setInput] = React.useState('');
+
+  const TODO_LIST_KEY = 'TODO_LIST_KEY';
+  const saveTodoList = (newTodoList: TodoItemType[]) => {
+    setTodoList(newTodoList);
+    AsyncStorage.setItem(TODO_LIST_KEY, JSON.stringify(newTodoList));
+  };
 
   const addTodo = () => {
     const len = todoList.length;
@@ -39,12 +46,13 @@ export default function useTodoList({ selectedDate }: useTodoListProps) {
       },
     ];
 
-    setTodoList(newTodoList);
+    saveTodoList(newTodoList);
   };
 
   const removeTodo = (todoId: number) => {
     const newTodoList = todoList.filter((todo) => todo.id !== todoId);
-    setTodoList(newTodoList);
+
+    saveTodoList(newTodoList);
   };
 
   const toggleTodo = (todoId: number) => {
@@ -57,7 +65,7 @@ export default function useTodoList({ selectedDate }: useTodoListProps) {
       };
     });
 
-    setTodoList(newTodoList);
+    saveTodoList(newTodoList);
   };
 
   const resetInput = () => {
@@ -68,6 +76,20 @@ export default function useTodoList({ selectedDate }: useTodoListProps) {
     const isSameDate = dayjs(todo.date).isSame(selectedDate, 'date');
     return isSameDate;
   });
+
+  React.useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    const result = await AsyncStorage.getItem(TODO_LIST_KEY);
+
+    if (result) {
+      const newTodoList = JSON.parse(result);
+
+      setTodoList(newTodoList);
+    }
+  };
 
   return {
     todoList,
